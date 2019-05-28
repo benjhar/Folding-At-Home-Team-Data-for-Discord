5b25ec2c87df20ab20f4435bb19c5b54dff7fe53
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2019-05-28 01:08:45 +0100

    Added leaderboard command

--- bot.py
+++ bot.py
@@ -5,8 +5,38 @@ import format
 import foldingathome as fah
 import time
 
-bot = commands.Bot(command_prefix='!@#$%^&*()')
+team_number = 235150
+channelA = 581941336580816917
+channelB = 581941356780584990
+channelC = 582710022128140314
+bot = commands.Bot(command_prefix='fold ')
 bot.remove_command('help')
+
+@bot.command(pass_context=True)
+async def leaderboard(ctx,donor=None):
+    if donor:
+        try:
+            stats = fah.donor_stats(team_number, donor)
+        except :
+            await ctx.message.channel.send("Something went wrong. You may have entered an invalid donor or there may be a problem reaching Folding@Home, please try again.\nIf this has happened before, please try again later.")
+
+        title = 'Folding@Home statistics for {}'.format(stats[0])
+        description = "Donor '{}'".format(donor)
+        embed = discord.Embed(title=title, description=description, color=0x4286f4)
+        embed.add_field(name="Total credits for team", value=stats[1], inline=False)
+        embed.add_field(name="Total work units completed for team", value=stats[2], inline=False)
+        await ctx.message.channel.send(embed=embed)
+    else:
+        stats = fah.teamstats(team_number)
+        description = 'Team {}'.format(stats["name"])
+        rank = "Rank out of {}".format(stats["total_teams"])
+        embed = discord.Embed(title="Folding@Home statistics", description=description, color=0x4286f4)
+        embed.add_field(name="Total credits", value=str(stats["credit"]), inline=False)
+        embed.add_field(name="Total work units", value=str(stats["wus"]), inline=False)
+        embed.add_field(name=rank, value=str(stats["rank"]), inline=False)
+        embed.add_field(name="Total number of donors", value=str(len(stats["donors"])), inline=False)
+        embed.set_thumbnail(url=stats["logo"])
+        await ctx.message.channel.send(ctx.message.channel, embed=embed)
 
 @bot.event
 async def on_member_join(member):
@@ -21,7 +51,7 @@ async def on_member_update(before, after):
     await update_count(await get_fah_stats())
 
 async def get_fah_stats():
-    team = fah.teamstats(235150)
+    team = fah.teamstats(team_number)
     highest_scorer = fah.highest_scorer(team)
     team_score = fah.team_score(team)
     team_wus = fah.team_work_units(team)
@@ -31,9 +61,9 @@ async def get_fah_stats():
 async def update_count(stats):
     hs, ts, twus = stats
 
-    await bot.get_channel(581980094373822484).edit(name=await format.convert_string(hs[0] + ' : ') + str(hs[1]))
-    await bot.get_channel(581980129182613505).edit(name=await format.convert_string('total score' + ' : ' + str(ts)))
-    await bot.get_channel(581980156244131856).edit(name=await format.convert_string('total wus' + ' : ' + str(twus)))
+    await bot.get_channel(channelA).edit(name=await format.convert_string(hs[0] + ' : ') + str(hs[1]))
+    await bot.get_channel(channelB).edit(name=await format.convert_string('total score' + ' : ' + str(ts)))
+    await bot.get_channel(channelC).edit(name=await format.convert_string('total wus' + ' : ' + str(twus)))
 
 @bot.event
 async def on_ready():
@@ -43,4 +73,4 @@ async def on_ready():
     print('------')
     await update_count(await get_fah_stats())
 
-bot.run('NTgxOTEzNjA0MDk0NDI3MTQ2.XOsBsg.eVefkxYotU-KE6ZF2Xh9sKeRJs8')
+bot.run('NTgyNjkxNTkwNzc5NDM3MDYz.XOxgAQ.C7iV5xPCxRboLm3OKH0A0rims1Q')
--- foldingathome.py
+++ foldingathome.py
@@ -4,6 +4,21 @@ def teamstats(teamnum):
     r = requests.get("https://stats.foldingathome.org/api/team/" + str(teamnum))
     return r.json()
 
+def donor_stats(teamnum, user):
+    team = teamstats(teamnum)
+    donors = team["donors"]
+    donorstats = []
+    found = False
+    for donor in donors:
+        if donor["name"] == user:
+            donorstats.append(user)
+            donorstats.append(donor["credit"])
+            donorstats.append(donor["wus"])
+            return donorstats
+    if not found:
+        raise "No user could be found with that name!"
+        
+
 def highest_scorer(team):
     donors = team["donors"]
     users_and_scores = {}
